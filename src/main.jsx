556eb263b7ac794c4fa81fd2b0cd382d9cede05e
Rashmi <rashmantri2003@gmail.com>
2024-08-12 20:54:35 +0530

    Added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import About from "./components/About.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const appRouter = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/about",
+		element: <About />,
+	},
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={appRouter} />
+	</React.StrictMode>
 )
