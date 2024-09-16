134c3e437c712b211be1b7f10aea729289a2d56c
Rashmi <rashmantri2003@gmail.com>
2024-09-16 19:06:07 +0530

    Updated firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,6 +6,7 @@ import { getAuth } from "firebase/auth";
 const {
   VITE_API_KEY,
   VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
   VITE_STORAGE_BUCKET,
   VITE_MESSAGE_SENDER,
   VITE_APP_ID,
@@ -16,7 +17,7 @@ const {
 const firebaseConfig = {
   apiKey: VITE_API_KEY,
   authDomain: VITE_AUTH_DOMAIN,
-  projectId: "foodzila-518bd",
+  projectId: VITE_PROJECT_ID,
   storageBucket: VITE_STORAGE_BUCKET,
   messagingSenderId: VITE_MESSAGE_SENDER,
   appId: VITE_APP_ID,
